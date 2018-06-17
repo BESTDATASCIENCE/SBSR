d5889fc5af69bcba9b1a0352a8d406c6c093b257
arturochian <arturob.chian@gmail.com>
2018-06-17 18:13:53 -0500

    Comenzando el paquete
    :copyright:

---
+++ R/SBSR.R
@@ -0,0 +1,18 @@
+#' Trabaja con mapas de Peru. Creado por BEST
+#'
+#' @name SBSR
+#' @docType package
+
+NULL
+
+#' Base de datos para trabajar data de los indicadores financieros de los bancos locales.
+#'
+#' Base de datos para trabajar data de los indicadores financieros de los bancos locales.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name BancosIndFin
+#' @usage data(BancosIndFin)
+#' @format Base de datos para trabajar data de los indicadores financieros de los bancos locales.
+#'
+NULL
