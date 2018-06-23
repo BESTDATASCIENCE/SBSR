33d9093eac00caf73f9489b1eb0ae68ef62081cc
arturochian <arturob.chian@gmail.com>
2018-06-23 09:44:20 -0500

    Actualizando messages

---
+++ R/zzz.R
@@ -0,0 +1,41 @@
+### File R/zzz.R
+### Part of the R package SBSR
+### Copyright Behavioral Economics and Data Science Team (BEST)
+
+### It is made available under the terms of the GNU General Public
+### License, version 2, or at your option, any later version,
+### incorporated herein by reference.
+###
+### This program is distributed in the hope that it will be
+### useful, but WITHOUT ANY WARRANTY; without even the implied
+### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+### PURPOSE.  See the GNU General Public License for more
+### details.
+###
+### You should have received a copy of the GNU General Public
+### License along with this program; if not, write to the Free
+### Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+### MA 02110-1301, USA
+
+
+.onLoad<- function(lib, pkg,...)
+
+.onAttach <- function(lib, pkg,...){
+    packageStartupMessage(BESTWelcomeMessage())
+}
+
+BESTWelcomeMessage <- function(){
+
+ paste("\n",
+        "Bienvenidos al paquete SBSR version ", packageDescription("SBSR")$Version, "\n",
+        "\n",
+        "Conozca más de los cursos, proyectos y otros servicios de BEST\n",
+        "en nuestra página web:\n",
+        "https://http://besteamperu.org/\n",
+       "y nuestra página de github:\n",
+       "https://github.com/BESTDATASCIENCE\n",
+        "\n",
+        "To suppress this message use:\n",
+        "suppressPackageStartupMessages(library(SBSR))\n",
+          sep="")
+}
